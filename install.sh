0d0cef045ecf5854d116f41193a555016ef296e6
haoflynet <haoflynet@gmail.com>
2015-12-04 14:00:01 +0800

    update

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
-#/bin/bash
+#!/bin/bash
 
 # author:haofly
 
 # 安装配置更新源
 version=`head -n 1 /etc/issue | awk '{print $1}'`
 version_number=`head -n 1 /etc/issue | awk '{print $2}' | awk -F '.' '{print $1$2}'`
+sources_name=`ls | grep "$vaersion" | grep "$versoin_number"`
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+cp ./sources.list/$sources_name /etc/apt/sources.list
 
+# 更新
+sudo apt-get update && sudo apt-get upgrade -y
 
+# VIM安装与配置
+sudo apt-get install vim -y
